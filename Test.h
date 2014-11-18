938fa7f21fac7aa7fe0a4c4cadc8cb0b73d92b10
mirazzaq <mirazzaq@gmail.com>
2014-11-18 15:12:08 +0500

    Added a test class

---
+++ Test.h
@@ -0,0 +1,13 @@
+//
+//  Test.h
+//  Git Test
+//
+//  Created by M Irfan on 18/11/2014.
+//  Copyright (c) 2014 NTES. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Test : NSObject
+
+@end
---
+++ Test.m
@@ -0,0 +1,13 @@
+//
+//  Test.m
+//  Git Test
+//
+//  Created by M Irfan on 18/11/2014.
+//  Copyright (c) 2014 NTES. All rights reserved.
+//
+
+#import "Test.h"
+
+@implementation Test
+
+@end
